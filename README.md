becc07354f3fab1faf673de731c4495c67c7b9cb
Bennett Perkins <1610227+bennettp123@users.noreply.github.com>
2024-12-17 11:39:41 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# advent-of-code-2024
+
+Codebase for https://adventofcode.com/2024
+
+Each day has two challenges.
+
+* Day 1 can be found in `packages/day-01-1` and `packages/day-01-2`, respectively.
+* Day 2 can be found in `packages/day-02-1` and `packages/day-02-2`, respectively.
+
+etc.
+
+To add a new day:
+
+```bash
+yarn run new-day
+```
+
---
+++ scripts/new-day.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+rootdir="$(dirname "$(dirname "$(readlink -f "${0}")")")"
+
+function get_current_day() {
+  (
+    cd "${rootdir}/packages"
+    shopt -s nullglob
+    for dir in day-*; do echo "$dir"; done \
+      | sed 's/^day-//' \
+      | sed 's/-[12]$//' \
+      | sort -r \
+      | head -n1
+  )
+}
+
+today="$(date +%d)" # eg 17
+current_day="$(get_current_day)"
+if [ -z "${current_day}" ]; then
+  current_day=0
+fi
+
+next_day="$(expr "${current_day}" + 1)"
+
+if ! [ -z "${1}" ]; then
+  desired_day="${1}"
+else
+  desired_day="${next_day}"
+  if [ ${desired_day} -gt ${today} ]; then
+    desired_day="${today}"
+  fi
+fi
+
+day="$(printf '%.2d' "${desired_day}")"
+
+if [ ${day} -lt 1 ] || [ ${day} -gt 99 ]; then
+  echo "Invalid day: ${day}" >&2
+  exit 1
+fi
+
+if [ -r "${rootdir}/packages/day-${day}-1" ] || \
+   [ -r "${rootdir}/packages/day-${day}-2" ]; then
+    echo "Day ${day} already exists" >&2
+    exit 1
+fi
+
+for num in 1 2; do
+  name="day-${day}-${num}"
+  package="@advent-of-code-2024/${name}"
+  packagedir="${rootdir}/packages/${name}"
+  
+  mkdir -p "${packagedir}"
+  ( cd "${packagedir}" && yarn init -p -n "${package}" )
+  echo "created ${package} in ${packagedir}"
+done
