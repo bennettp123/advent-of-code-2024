6ef8395fc15f20eb0592f671c9188488808bbcd3
Bennett Perkins <1610227+bennettp123@users.noreply.github.com>
2025-01-07 16:16:01 +0800

    delete broken test

--- packages/day-05/index.test.ts
+++
@@ -1,6 +0,0 @@
-import { describe, expect, it } from '@jest/globals'
-describe('index.mjs', () => {
-    it('should resolve', async () => {
-        expect(await import('./index.ts')).toBeDefined()
-    })
-})
